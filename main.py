7c54300b395133dace18c7114b7b44e23a621388
Calorine06 <ac6819485@gmail.com>
2026-09-03 13:06:46 -0300

    corrige escopo do app no main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
-from produtos import LISTA_PRODUTOS  # Importa a lista dos seus doces
 
+# 1. O app DEVE ser criado aqui no escopo principal:
 app = Flask(__name__)
+
+LISTA_PRODUTOS = [...] # suas rotas e produtos aqui
 
 @app.route('/')
 def home():
-    # Envia a lista dos doces para a página HTML automaticamente
     return render_template('login.html', produtos=LISTA_PRODUTOS)
 
+# 2. Apenas o app.run fica no final:
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
