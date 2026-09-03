0333b99b9bbec7d9f24e2ea34b78a7d4535f626c
Calorine06 <ac6819485@gmail.com>
2026-09-03 13:24:19 -0300

    fix: atualiza templates e main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from flask import Flask, render_template
 # 1. O app DEVE ser criado aqui no escopo principal:
 app = Flask(__name__)
 
-LISTA_PRODUTOS = [...] # suas rotas e produtos aqui
+LISTA_PRODUTOS = [
+    {"nome": "Cone Trufado", "preco": 12.00},
+    {"nome": "Palha Italiana", "preco": 15.49}, 
+    {"nome": "Tortinha", "preco": 16.00  },
+    {"nome": "Bombom de Morango", "preco": 18.00},
+    {"nome": "Bombom de Uva", "preco": 18.00},
+] # suas rotas e produtos aqui
 
 @app.route('/')
 def home():
